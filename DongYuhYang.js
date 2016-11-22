1109fb2c3a0ee96044b670b30a3c2dc497272c3a
aa002868 <aa002868@au.edu.tw>
2016-11-22 23:40:58 +0800

    Update DongYuhYang.js

--- DongYuhYang.js
+++ DongYuhYang.js
@@ -4,7 +4,7 @@ d3.csv("https://aa002868.github.io/pdata/DongYuhYang.csv", function(data) {
 	console.log(data);
 	      d3.wordcloud()
         .size([800, 400])
-        .selector('#wordcloud')
+        .selector('#DongYuhYang')
         .words(data)
         .start();
 });
