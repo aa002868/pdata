91141ee005475a6f42ef798e1d1323eedc14bfe0
aa002868 <aa002868@au.edu.tw>
2016-11-22 23:42:23 +0800

    Update JouMingChang.js

--- JouMingChang.js
+++ JouMingChang.js
@@ -4,7 +4,7 @@ d3.csv("https://aa002868.github.io/pdata/JouMingChang.csv", function(data) {
 	console.log(data);
 	      d3.wordcloud()
         .size([800, 400])
-        .selector('#wordcloud')
+        .selector('#JouMingChang')
         .words(data)
         .start();
 });
