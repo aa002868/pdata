182eebe678c470045b6f30fa6ba315ecca5f3dac
aa002868 <aa002868@au.edu.tw>
2016-11-22 23:41:50 +0800

    Update HungLungWang.js

--- HungLungWang.js
+++ HungLungWang.js
@@ -4,7 +4,7 @@ d3.csv("https://aa002868.github.io/pdata/HungLungWang.csv", function(data) {
 	console.log(data);
 	      d3.wordcloud()
         .size([800, 400])
-        .selector('#wordcloud')
+        .selector('#HungLungWang')
         .words(data)
         .start();
 });
