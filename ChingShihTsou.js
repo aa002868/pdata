5ca0a703507f861dd75cb3af4a4ee899c7b2a9d8
aa002868 <aa002868@au.edu.tw>
2016-11-30 20:36:13 +0800

    Update ChingShihTsou.js

--- ChingShihTsou.js
+++ ChingShihTsou.js
@@ -3,7 +3,7 @@ function ChingShihTsou(){
 d3.csv("https://aa002868.github.io/pdata/ChingShihTsou.csv", function(data) {
 	console.log(data);
 	      d3.wordcloud()
-        .size([600, 300])
+        .size([100%, 100%])
         .selector('#ChingShihTsou')
         .words(data)
         .start();
